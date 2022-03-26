c49e19870dcb6e39d63b2f98007a2bffaf293ebc
dammatsu.yuta <dammatsu.yuta@moneyforward.co.jp>
2022-03-26 13:12:39 +0900

    done

--- client.rb
+++ client.rb
@@ -2,22 +2,49 @@
 
 require_relative 'pb/sample_services_pb'
 
-def main(input)
-  stub = Example::Calc::Stub.new('localhost:50051', :this_channel_is_insecure)
-  output = stub.solve(Example::Input.new(question: input))
-  p "答えは #{output.answer}"
+STUB = Example::Calc::Stub.new('localhost:50051', :this_channel_is_insecure)
+
+def upload(file_path)
+  file = File.open(file_path)
+
+  # リクエストを作成
+  request = Sample::UploadRequest.new
+
+  # リクエストに値を詰めていく
+  request.file_name = File.basename(file.path)
+  request.file_blob = file.read
+
+  # スタブにリクエストを入れて、gRPC サーバーの rpc を呼び出す。
+  response = STUB.upload(request)
+
+  if response.error == 'NO_ERROR'
+    puts "アップロード完了 file_name=#{file_name}, created_at=#{response.created_at}"
+  else
+    puts "アップロード失敗 file_name=#{file_name}, error=#{response.error}"
+  end
 end
 
-p '計算式を書いてください'
-input = gets.chomp
+def download(file_name)
+  # リクエストを作成
+  request = Sample::UploadRequest.new
 
-main(input)
-__END__
+  # リクエストに値を詰めていく
+  request.file_name = file_name
 
-file_path = ARGV[0]
+  # Stub にリクエストを入れて、gRPC サーバーの rpc を呼び出す。
+  response = STUB.download(request)
 
-f = File.open(file_path)
-file_name = File.basename(f.path)
-file_blob = f.read
+  if response.error == 'NO_ERROR'
+    File.open(Dir.home + "/Desktop/#{file_name}", 'w') { |f| f.write(response.file_blob) }
+    puts "ダウンロード成功 file_name=#{file_name}"
+  else
+    puts "ダウンロード失敗 file_name=#{file_name}, error=#{response.error}"
+  end
+end
 
-puts file_name, file_blob
+method = ARGV[0]
+file_name = ARGV[1]
+
+raise ArgumentError if %w[upload download].exclude?(method) || file_name.nil?
+
+send(method, file_name_or_path)
