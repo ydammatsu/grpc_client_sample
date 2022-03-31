cd442eb83acaf75b72d89ce4b7e3b69759427251
dammatsu.yuta <dammatsu.yuta@moneyforward.co.jp>
2022-03-31 17:52:40 +0900

    fix

--- bin/client.rb
+++ bin/client.rb
@@ -49,7 +49,7 @@ def download(file_name)
   file_blob = Base64.decode64(response.file_blob)
 
   if response.error == :NO_ERROR
-    File.open(Dir.home + "/Desktop/#{file_name}", 'w') { |f| f.write(file_blob) }
+    File.open(Dir.home + "/Desktop/ここにダウンロードします/#{file_name}", 'w') { |f| f.write(file_blob) }
     puts "ダウンロード成功 file_name=#{file_name}"
   else
     puts "ダウンロード失敗 file_name=#{file_name}, error=#{response.error}"
